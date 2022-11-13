2f2793331ebca28e15bf88c79918971e100c97ea
Alexis <63512507+Aldbg74@users.noreply.github.com>
2022-11-13 11:52:34 +0100

    Add files via upload

---
+++ Creepy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while :
+do
+    sleep 200
+	open "https://i.redd.it/0cha3mm5f7x21.jpg"
+	sleep 80
+    open "https://static.wikia.nocookie.net/creepypasta/images/8/8c/26627919_170x100.gif/revision/latest?cb=20200817201037&path-prefix=fr"
+done
+
+# (c) adrago-b  2021
