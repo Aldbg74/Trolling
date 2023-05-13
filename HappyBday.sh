e390cad2d70d8a23126e24dfb3ae7f225f7fa55e
Alexis <63512507+Aldbg74@users.noreply.github.com>
2023-05-13 16:36:05 +0200

    Add files via upload

---
+++ HappyBday.sh
@@ -0,0 +1,13 @@
+# Happy Bday
+# Alexis.D
+# 2023
+
+while true ; do
+    echo "Happy Birthday to you"
+    open "https://www.youtube.com/watch?v=dNEnzdcnWzE"
+    echo "Happy Birthday to you"
+    echo "Happy Birthday dear $1"
+    echo "Happy Birthday to you"
+    echo " "
+    sleep 1
+done
